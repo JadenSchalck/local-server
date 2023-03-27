c5bb3bb961a5f1b9f75207b5ccb89ba077d5995c
Jaden <105829789+JadenSchalck@users.noreply.github.com>
2023-03-27 07:12:05 -0400

    Unfinished

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 //3456789A123456789B123456789C123456789D123456789E123456789F123456789G123456789H
-
+var money = 0
 var power = 1
 
 //increase the amount of moneywhen clicked
 function buttonPressed(){
-    console.log("button pressed")
-    document.getElementById("money").innerHTML = 
-        document.getElementById("money").innerHTML.substring(0, 1) + 
-        (parseFloat(document.getElementById("money").innerHTML.substring(1)) + power);
+    console.log("button pressed");
+    money += power;
+    var text = document.getElementById("money").innerHTML;
+    console.log(document.getElementById("money").innerHTML);
+    console.log(text.substring(0, 9));
+    console.log(text.substring(9 + money.toString().length));
+    text = text.substring(0, 9) + 
+        (money + power) + text.substring(9 + money.toString().length);
 }
 
 function upgradePower(){
