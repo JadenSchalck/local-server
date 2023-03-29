4fdea323661c8c3fa437236678557e33fbb49f9a
Jaden <105829789+JadenSchalck@users.noreply.github.com>
2023-03-29 18:11:13 -0400

    Added cats
    You can buy and sell them. The price fluctuates. Buy low, sell high. Take this little kitty to the moon.

--- computation.js
+++ computation.js
@@ -1,6 +1,8 @@
 //3456789A123456789B123456789C123456789D123456789E123456789F123456789G123456789H
-
+//money
 var money = 0;
+
+//power
 var power = 1;
 var powerCost = 10;
 var powerCostScaling = 5;
@@ -25,5 +27,45 @@ function upgradePower(){
         updatePower();
     }else{
         alert("That's too expenive!");
+    }
+}
+
+//cats
+var cats = 0
+var catCost = 100
+
+//make cat price fluctuate. called once html is loaded.
+function changeCatPrice(){
+    console.log("Price is changing.")
+    catCost += Math.random() - .5;
+    catCost = Math.round(catCost * 100) / 100;
+
+    if (catCost > 1000){
+        catCost = 1000;
+    }else if (catCost < 0){
+        catCost = 0;
+    }        updateCatPrice();
+}
+setInterval(changeCatPrice, 1000);
+
+function buyCat(){
+    if (money >= catCost){
+        money -= catCost;
+        cats++;
+        updateMoney();
+        updateCats();
+    }else{
+        alert("That's too expenive!");
+    }
+}
+
+function sellCat(){
+    if (cats > 0){
+        cats--;
+        money += catCost;
+        updateMoney();
+        updateCats();
+    }else{
+        alert("You don't have any cats to sell!");
     }
 }
