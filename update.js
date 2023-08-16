6707235a1bee159139de336b61fe6595196213cd
Jaden <105829789+JadenSchalck@users.noreply.github.com>
2023-08-16 15:52:56 -0400

    Added spinning gorillas
    The faster they spin the more you earn. Pay to reduce friction.

--- update.js
+++ update.js
@@ -1,12 +1,19 @@
 function updateAll(){
+    //updateSettings();
     //updateMainButton();
     updateMoney();
     updatePower();
+
     updateCatPrice();
     updateCats();
+
+    updateFrictionPerSecond();
+    updateRotationSpeed();
+    updateReduceFrictionButton();
+    updateGorillaVideo();
 }
 
-
+//unused
 function updateSettings() {
     document.getElementById("saveCodeOutput").innerHTML = saveCode;
     console.log(saveCode);
@@ -14,18 +21,21 @@ function updateSettings() {
 
 //unused
 function updateMainButton() {
-    document.getElementById("mainButton").value = document.getElementById("mainButton").value.substring(0, 10) + power.toFixed(2);
+    document.getElementById("mainButton").value = document.getElementById("mainButton").value.substring(0, 11) + power.toFixed(2);
 }
+
 
 function updateMoney() {
     document.getElementById("money").innerHTML = document.getElementById("money").innerHTML.substring(0, 1) + money.toFixed(2);
 }
 
 function updatePower() {
-    document.getElementById("mainButton").value = document.getElementById("mainButton").value.substring(0, 10) + power.toFixed(2);
+    document.getElementById("mainButton").value = document.getElementById("mainButton").value.substring(0, 11) + power.toFixed(2);
     document.getElementById("clickPower").value = document.getElementById("clickPower").value.substring(0, 40) + powerCost;
 }
 
+
+//cats
 function updateCatPrice() {
     //console.log(document.getElementById("catsPrice").innerHTML);
     document.getElementById("catsPrice").innerHTML = 
@@ -34,4 +44,23 @@ function updateCatPrice() {
 
 function updateCats() {
     document.getElementById("catsAmount").innerHTML = document.getElementById("catsAmount").innerHTML.substring(0, 11) + cats;
+}
+
+
+//gorilla
+function updateFrictionPerSecond() {
+    document.getElementById("frictionPerSecond").innerHTML = document.getElementById("frictionPerSecond").innerHTML.substring(0, 17) + frictionPerSecond;
+}
+
+function updateRotationSpeed() {
+    document.getElementById("rotationSpeed").innerHTML = document.getElementById("rotationSpeed").innerHTML.substring(0, 16) + gorillaSpinMult.toFixed(2);
+}
+
+function updateReduceFrictionButton() {
+    document.getElementById("reduceFrictionButton").value = document.getElementById("reduceFrictionButton").value.substring(0, 25) + reduceFrictionCost;
+}
+
+function updateGorillaVideo() {
+    //console.log(gorillaSpinMult);
+    document.querySelector("video").playbackRate = gorillaSpinMult;
 }
